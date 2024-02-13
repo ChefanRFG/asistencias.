024dc7bd45425e3f334ceac02f6c6a3c30101d80
ChefanRFG <158303850+ChefanRFG@users.noreply.github.com>
2024-02-13 09:21:39 -0600

    Add files via upload

---
+++ asistencias.py
@@ -0,0 +1,17 @@
+alumno=" "
+nombre=" "
+apellido=" "
+carne=0
+nombre = input("ingrese su nombre: ")
+for _ in range(10):
+    print("ingrese su nombre: ")
+
+apellido = input("ingrese su apellido: ")
+for _ in range(10):
+    print("ingrese su apellido: ")
+
+carne = float(input("ingrese su carne: "))
+for _ in range(10):
+    print("ingrese su carne: ")
+
+alumno = nombre + apellido + carne
